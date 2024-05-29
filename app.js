8db5e86aee7d235daf17963197f8511728b0a8c5
0zodbek <ozpdbekov@gmail.com>
2024-05-29 12:04:12 +0500

    project

---
+++ app.js
@@ -0,0 +1,138 @@
+// 1-masala
+
+// alert("1 dan 50 gacha sonlar orasida toq sonlarning kvadralarini chiqarish;");
+
+// let n = 50 ;
+
+// for(let i = 1 ; i <= n ; i+=2){
+// console.log(i*i);
+// }
+
+
+// 2-masala
+
+// alert("1 dan 10 gacha sonlarning kublarini chiqarish;");
+
+// let n = 10 ;
+
+// for(let i = 1 ; i <= n ; i++){
+// console.log(i*i*i);
+// }
+
+
+// 3-masala
+
+// alert("1 dan 20 gacha sonlarning faktorilini chiqarish;");
+
+// let n = 20 ;
+
+// let sum = 0 ;
+
+// for(let i = 1 ; i <= n ; i++){
+// console.log(sum+i);
+// }
+
+
+// 4-masala
+
+// alert("1 dan 30 gacha bo'lgan sonlarning kvadratini yig'indisini topish;");
+
+// let a = 30 ;
+
+// let sum = 0 ;
+
+// for(let i = 1 ; i <= a ; i++){
+// sum += i*i;
+// }
+
+// console.log(sum);
+
+
+// 5-masala
+
+// alert("1 dan 20 gacha bolgan sonlarning tublarini chiqarish;");
+
+// let n = +prompt("");
+
+// let counter = 0 ;
+
+// let num = 0 ;
+
+// for(let i = 1 ; i <= n ; i++){
+// if(n % i == 0 ){
+//     counter = counter + 1;
+    
+// }
+// if (counter > 2 ){
+
+// num = i ;
+
+// console.log(num);
+// }
+// };
+
+
+
+// 6-masala
+
+// alert("1 dan 100 gacha bolgan sonlardan 2 ga kopaytirganda 100 dan kichigini topish;");
+
+// let a = 100 ; 
+
+// let sum = 0 ; 
+
+// for(let i = 1 ; i*2 < a ; i++){
+// console.log(i);
+// }
+
+
+// 7-masala
+
+// alert("1 dan 100 gacha bolgan sonlarni orasida 7 ga bo'linadigan 5 ga bo'linmaydigan sonlarni topish;");
+
+// let a = 100 ;
+
+// for(let i = 1 ; i <= 100 ; i++){
+// if(i % 7 == 0 && i % 5 != 0){
+// console.log(i);
+// };
+// };
+
+
+// 8-masala
+
+// alert("berilgan sonning barcha boluvchilarini va yig'indisini topish;");
+
+// let a = 100 ;
+
+// let sum = 0 ;
+
+// for(let i = 1 ; i <= a ; i++ ){
+// if(a % i == 0){
+// console.log(i)
+// sum += i ;
+// }
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// qoshimcha uy ishi 
+
+
+
+
+// 1 
